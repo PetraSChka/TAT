40cafb3c062cf945ab416acd23aaa9243e50535b
Artem Petraschenko <petraschkaprod@gmail.com>
2020-11-21 15:40:46 +0300

    fix

--- Lab7/src/test/java/WebDriverAmazonSorting.java
+++ Lab7/src/test/java/WebDriverAmazonSorting.java
@@ -1,18 +1,11 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.opera.OperaDriver;
-
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -39,11 +32,6 @@ public class WebDriverAmazonSorting {
                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"s-result-sort-select_1\"]")));
         sortByFromLowToHighPrice.click();
 
-        List <WebElement> sortElements = driver.findElements(By.xpath("//span[@class='a-price-whole'"));
-        int size= sortElements.size();
-        for (int i=0; i<size; i++){
-            if(sortElements.get(i)<)
-        }
         driver.quit();
     }
 
@@ -68,7 +56,10 @@ public class WebDriverAmazonSorting {
         WebElement sortByFromLowToHighPrice = wait
                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"s-result-sort-select_1\"]")));
         sortByFromLowToHighPrice.click();
-        Assert.assertTrue(sortByFromLowToHighPrice.);
+
+        List <WebElement> sortElements = driver.findElements(By.xpath("//span[@class='a-price-whole'"));
+        int size= sortElements.size();
+
         driver.quit();
     }
 }
